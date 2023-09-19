4d297587dce74f2824ebb166ab2a883d76f383b7
BeratDM <check_point2@hotmail.com>
2023-09-19 17:03:31 +0300

    base btmanager created

--- app/btmanager.py
+++ app/btmanager.py
@@ -2,13 +2,49 @@ import config, bclient, btrader
 
 
 class BTManager():
-
-    def __init__(self, myClient: bclient.MyClient) -> None:
+    # python 3.9 enables to use list[str] instead of List[str] from typing module
+    def __init__(self, myClient: bclient.MyClient, TRADE_SYMBOLS : list[str], TRADE_INTERVALS : list[str]) -> None:
         
-        pass
+        self.TRADE_SYMBOLS = TRADE_SYMBOLS
+        self.TRADE_INTERVALS = TRADE_INTERVALS
+        self.myTraders = list(btrader.bTrader)
+        self.myTraders_index_info = dict()
 
-    def cache_TradeInfo(self):
-        pass
+        try:
+            self.create_traders_from_env()
+            self.start_all_traders()
+        except Exception as e:
+            print(e)
+
+
+
+    def create_trader(self, TRADE_SYMBOL: str, TRADE_INTERVAL: str) -> int:
+        self.myTraders.append(btrader.bTrader(myClient=self.myClient, TRADE_SYMBOL=TRADE_SYMBOL, TRADE_INTERVAL=TRADE_INTERVAL))
+        index = len(self.myTraders) - 1
+        self.myTraders_index_info[str(index)] = f"{TRADE_SYMBOL}+{TRADE_INTERVAL}+DefaultStrategy1"
+        return index
+
+
+    def start_trader(self, index: int):
+        if self.myTraders[index] is not None:
+            self.myTraders[index].start()
+
+
+    def stop_trader(self, index: int):
+        if self.myTraders[index] is not None:
+            self.myTraders[index].stop()
+
+
+    def create_traders_from_env(self):
+        i = 0
+        for ts in self.TRADE_SYMBOLS:
+            if self.TRADE_INTERVALS[i] is not None:
+                self.create_trader(TRADE_SYMBOL=ts, TRADE_INTERVAL= self.TRADE_INTERVALS[i])
+            i += 1
+
+    def start_all_traders(self):
+        for bt in self.myTraders:
+            bt.start()
 
     
 #my_btrader1 = btrader.bTrader(myClient=myClient, TRADE_SYMBOL=config.Trade_Info.TRADE_SYMBOLS[0], TRADE_INTERVAL=config.Trade_Info.TRADE_INTERVALS[0])
--- app/btrader.py
+++ app/btrader.py
@@ -4,6 +4,7 @@ from binance.enums import *
 import csv
 import config, RSI_Trade01, order_actions, khistory
 import asyncio, websockets, threading
+from typing import Callable
 
 #async def create_and_start_btrader(myClient, TRADE_SYMBOL, TRADE_INTERVAL):
 #    btrader = bTrader(myClient, TRADE_SYMBOL, TRADE_INTERVAL)
@@ -11,11 +12,6 @@ import asyncio, websockets, threading
 
 
 class bTrader():
-
-    async def the_task(self):
-        await self.start_websocket()
-
-
     def __init__(self, myClient, TRADE_SYMBOL, TRADE_INTERVAL):
         self.myClient = myClient
         self.TRADE_SYMBOL = TRADE_SYMBOL
@@ -24,12 +20,8 @@ class bTrader():
         self.SOCKET = "wss://stream.binance.com:9443/ws/{}@kline_{}".format(TRADE_SYMBOL.lower(), TRADE_INTERVAL)
         self.init_closes()
 
-        #loop = asyncio.get_event_loop()
-        #loop.create_task(self.the_task())
-
-        threading.Thread(target=self.start_websocket).start()
-        
-        #asyncio.run(self.the_task())
+        self.websocket_handler = WebSocketHandler(self.SOCKET, self.on_message, self.print)
+        #self.start_websocket()
         
         
 
@@ -45,65 +37,17 @@ class bTrader():
         self.print(self.closes) 
         self.print("a bTrader instance is initiated with {} starting values.".format(len(self.closes)))
 
+
     def print(self, msg : str):
         print(str(self.TRADE_SYMBOL) + "-" + str(self.TRADE_INTERVAL) + ": " + str(msg))
 
 
-    def start_websocket(self):
-        
-        '''
-        def on_open(ws):
-            print("opened connection")
+    def start(self):
+        self.websocket_handler.start()
 
 
-        def on_close(ws):
-            print("closed connection")
-
-
-        def on_message(ws, message):
-            
-            #global np_closes
-
-            self.print("received message")
-            json_message = json.loads(message)
-            #pprint.pprint(json_message)
-
-            #look-up the payload of the websocket stream on here https://github.com/binance/binance-spot-api-docs/blob/master/web-socket-streams.md  
-            candle = json_message["k"]
-
-            is_candle_closed = candle['x']
-            price_closed = candle['c']
-            self.print(price_closed)
-            #initiate logic upon new candle information.
-            if is_candle_closed:
-                self.print("candle closed at {}".format(price_closed))
-                self.closes.append(float(price_closed))
-                print("closes")
-                self.print(self.closes)
-                print(len(self.closes))
-
-                self.new_candle_closed()
-        '''
-        #Get updates from socket.
-        #ws = websocket.WebSocketApp(self.SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
-        #ws.run_forever()
-
-        # https://betterprogramming.pub/websockets-and-asyncio-beyond-5-line-samples-part-1-ddf8699a18ce
-        def websocket_loop():
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-
-            conn = websockets.connect(uri= self.SOCKET)
-
-            async def inner_websocket_loop():
-                async with conn as ws: 
-                    while True:
-                        message = await ws.recv()
-                        self.on_message(message)
-
-            loop.run_until_complete(inner_websocket_loop())
-
-        threading.Thread(target=websocket_loop).start()
+    def stop(self):
+        self.websocket_handler.stop()
 
 
     def on_message(self, message):
@@ -134,3 +78,43 @@ class bTrader():
     def new_candle_closed(self):
         print("this should only triggers only once for every message on this instance")
         #RSI_Trade01.calculate_trade(client = self.myClient.client, closes = self.closes)
+
+
+class WebSocketHandler:
+    def __init__(self, SOCKET : str, on_message : Callable[[str], None], report_error_str: Callable[[str], None]) -> None:
+        self.SOCKET = SOCKET
+        self.on_message = on_message
+        self.report_error_str = report_error_str
+        self.stop = False
+        self.thread = None
+
+    
+    def start(self):
+        self.thread = threading.Thread(target=self.websocket_loop)
+        self.thread.start()
+
+    
+    def stop(self):
+        self.stop = True
+        if self.thread is not None:
+            self.thread.join()
+            self.thread = None
+
+    
+    def websocket_loop(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        conn = websockets.connect(uri = self.SOCKET)
+
+        async def inner_websocket_loop():
+            async with conn as ws:
+                while not self.stop:
+                    try:
+                        message = await ws.recv()
+                        self.on_message(message)
+                    except Exception as e:
+                        self.report_error_str(f"An error occurred: {e}")
+                        break
+
+        loop.run_until_complete(inner_websocket_loop())
